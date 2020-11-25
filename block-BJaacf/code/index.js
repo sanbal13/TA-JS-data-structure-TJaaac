27d6877423ba687181740c861dfc6078dd378d73
Santosh Balchandran <santoshbalchandran@gmail.com>
2020-11-25 09:40:55 +0530

    Complete the exercise on Array Iteration

--- block-BJaacf/code/index.js
+++ block-BJaacf/code/index.js
@@ -1,19 +1,66 @@
 // 1. Create an array named numbers and store 5 number values in it
+let numbers = [1, 2, 3, 4, 5];
 
 // 2. Calculate the sum of array items and print it to the console using console.log()
+let sum = 0;
+for(let number of numbers) {
+    sum += number;
+}
+console.log('Sum: ${sum}');
 
 // 3. Calculate the average of array items and print it to the console using console.log()
+let avg = sum/(numbers.length);
+console.log('Average: ${avg}');
 
 // 4. Find the highest number in the array and print it to the console using console.log()
+let highest = numbers[0];
+for(let number of numbers) {
+    if(number > highest) {
+        highest = number;
+    }
+}
+console.log('Highest: ${highest}');
 
 // 5. Find the lowest number in the array and print it to the console using console.log()
+let lowest = numbers[0];
+for(let number of numbers) {
+    if(number < lowest) {
+        lowest = number;
+    }
+}
+console.log('Lowest: ${lowest}');
+
 
 // 6. Find the even numbers in the array and print them to the console using console.log()
 
+for(let number of numbers) {
+    if(number % 2 == 0) {
+        console.log(number);
+    }
+}
+
 // 7. Find the odd numbers in the array and print them to the console using console.log()
+for(let number of numbers) {
+    if(number % 2 != 0) {
+        console.log(number);
+    }
+}
 
 // 8. Find the numbers in the array that is divisible by 5 and print them to the console using console.log()
+for(let number of numbers) {
+    if(number % 5 == 0) {
+        console.log(number);
+    }
+}
 
 // 9. Log all the element of the array one by one
+for(let number of numbers) {
+    console.log(number);    
+}
 
 // 10. Find all the number in the array that is divisible by 3
+for(let number of numbers) {
+    if(number % 3 != 0) {
+        console.log(number);
+    }
+}
